f2d28981eeea6436e1924dd339a1c5d44ae8b008
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-25 19:38:51 +0000

    amend env file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const apiUrl = process.env.API_URL;
 const cors = require("cors");
 const giftRoutes = require("./routes/gifts");
 const userRoutes = require("./routes/users");
@@ -14,5 +15,5 @@ app.use("/gifts", giftRoutes);
 app.use("/users", userRoutes);
 
 app.listen(port, () => {
-  console.log("Server running on " + port);
+  console.log("Server running on " + apiUrl + port);
 });
