1d547effa5c9d54f3e5fc4b89532fc4b6361de82
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-26 10:22:00 +0000

    add patch address endpoint

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const crypto = require("crypto");
 
 function readUsers() {
   const file = fs.readFileSync("./data/users.json");
@@ -24,6 +23,31 @@ router.get("/:id", (req, res) => {
     return;
   }
   res.json(singleUser);
+});
+
+router.patch("/:id", (req, res) => {
+  try {
+    const data = readUsers();
+    const singleUser = data.find((user) => user.id === req.params.id);
+
+    if (!singleUser) {
+      res.status(404).send("User not found");
+    }
+    const updatedData = data.filter((user) => user.id !== req.params.id);
+
+    const updatedUser = {
+      ...singleUser,
+      address1: req.body.address1,
+      address2: req.body.address2,
+    };
+
+    updatedData.push(updatedUser);
+    const stringedUser = JSON.stringify(updatedData);
+    fs.writeFileSync("./data/users.json", stringedUser);
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Unable to update address", error });
+  }
 });
 
 module.exports = router;
